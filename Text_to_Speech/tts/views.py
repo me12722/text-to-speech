fff9f688f447efe286d9c59e102cc50966ba6e80
Ivan Gonzalez <Ivanglez43@gmail.com>
2016-11-17 22:55:33 -0800

    figured out how to get instances needed to create the mp3 file

--- Text_to_Speech/tts/views.py
+++ Text_to_Speech/tts/views.py
@@ -15,6 +15,10 @@ def index(request):
             aform.file_name = aform.cleaned_data['file_name']
             aform.text = aform.cleaned_data['text']
             aform.save()
+
+            # the exists but not in modelform, it exists in MP3 model!!!!
+            # hopefully this will help pave the way to a productive friday!
+            # :)!!!
             return render(request, 'tts/createmp3.html',None, {'form': aform})
         else:
             aform = Mp3DetailsForm()
@@ -26,7 +30,7 @@ def index(request):
         context = RequestContext(request)
         return render(request, 'tts/index.html', {'form':form})
 
-
+# in the follwing method make a call to the pk of MP3 and then print the details!
 def createMP3(request):
     if request.method == 'GET':
         print("we are here!")
