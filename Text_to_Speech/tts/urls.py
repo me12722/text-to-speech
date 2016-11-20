fd6b114de7bcd270a42f1e7c6a12464cd35180dd
Ivan Gonzalez <Ivanglez43@gmail.com>
2016-11-19 16:40:03 -0800

    v1.0 is done

--- Text_to_Speech/tts/urls.py
+++ Text_to_Speech/tts/urls.py
@@ -4,5 +4,4 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^createmp3/', TemplateView.as_view(template_name='tts/createmp3.html')),
 ]
--- Text_to_Speech/tts/views.py
+++ Text_to_Speech/tts/views.py
@@ -1,21 +1,37 @@
-from django.shortcuts import render, render_to_response, redirect,get_object_or_404
+from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
-from django.template import loader
 from .forms import Mp3DetailsForm
-from .models import MP3
+
+from gtts import gTTS
+
+import os
+import time
+
 
 def index(request):
-    template = None
-    aform = None
+
     if request.method == 'POST':
         context = RequestContext(request)
         aform = Mp3DetailsForm(request.POST)
+
         if aform.is_valid():
-            file_name = aform.cleaned_data['file_name']
+            file_name = aform.cleaned_data['file_name'].replace(" ", "")
             text = aform.cleaned_data['text']
-            aform.save()
-            return render(request, 'tts/createmp3.html', {'file_name': file_name, 'text':text})
+            create_sound(file_name, text)
+            file_path = '/Users/ivanG/Documents/text_to_speech/Text_to_Speech/' + file_name + ".mp3"
+
+            while not os.path.exists(file_path):
+                time.sleep(1)
+                if os.path.exists(file_path):
+                    break;
+
+            print("file about to be opened")
+            fsock = open(file_path, 'r')
+            response = HttpResponse(fsock, content_type='audio/mpeg')
+            response['Content-Disposition'] = "attachment; filename=%s.mp3" % file_name
+            return response
+
         else:
             aform = Mp3DetailsForm()
 
@@ -24,18 +40,11 @@ def index(request):
     else:
         form = Mp3DetailsForm()
         context = RequestContext(request)
-        return render(request, 'tts/index.html', {'form':form})
+        return render(request, 'tts/index.html', {'form': form})
 
-#the following function may not be necessary, woops
-# in the follwing method make a call to the pk of MP3 and then print the details!
-def createMP3(request):
-    if request.method == 'GET':
-        print("we are here!")
-        print(request)
-        return HttpResponse("we handled the Get! " )
-    if request.method == 'POST':
-        print("we are posting son")
-        return HttpResponse("we handled the Get! ")
 
-    template = loader.get_template('tts/createmp3.html')
-    return  HttpResponse(template.render(request))
+def create_sound(file_name, text):
+    txt = text
+    tts = gTTS(text=txt, lang='en')
+    tts.save(file_name + ".mp3")
+    return tts
